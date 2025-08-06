851d886730f4c5285fb67100d830eb38ab8fd6e4
MCDP <mcdp2alh@gmail.com>
2025-08-06 10:14:04 +0800

    配置 Input System

--- Assets/_Scripts/SpaceFighterController.cs
+++ Assets/_Scripts/SpaceFighterController.cs
@@ -1,9 +1,71 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace _Scripts
 {
-    public class SpaceFighterController : MonoBehaviour
+    [RequireComponent(typeof(Rigidbody))]
+    public class SpaceFighterController : MonoBehaviour, InputSystem_Actions.IPlayerActions
     {
-        
+        private InputSystem_Actions _actions;
+
+        private void Awake()
+        {
+            _actions = new InputSystem_Actions();
+            _actions.Player.AddCallbacks(this);
+        }
+
+        private void OnEnable()
+        {
+            _actions.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _actions.Player.Disable();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+        }
+
+        public void OnPrevious(InputAction.CallbackContext context)
+        {
+        }
+
+        public void OnNext(InputAction.CallbackContext context)
+        {
+        }
+
+        public void OnLaser(InputAction.CallbackContext context)
+        {
+        }
+
+        public void OnProjectile(InputAction.CallbackContext context)
+        {
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+        }
+
+        public void OnAccelerate(InputAction.CallbackContext context)
+        {
+        }
+
+        public void OnDecelerate(InputAction.CallbackContext context)
+        {
+        }
+
+        public void OnMenu(InputAction.CallbackContext context)
+        {
+        }
+
+        public void OnTool(InputAction.CallbackContext context)
+        {
+        }
     }
 }
